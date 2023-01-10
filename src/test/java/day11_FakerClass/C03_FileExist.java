d6d2884cb5d2f3ff44905864188b19b13a403deb
Hilalnigarbasan <hilalnigarbasan@outlook.com>
2023-01-10 18:50:47 +0300

    09.01.23

--- src/test/java/day11_FakerClass/C03_FileExist.java
+++ src/test/java/day11_FakerClass/C03_FileExist.java
@@ -1,4 +1,4 @@
-package day11_FakerClass;
+package day11;
 import org.junit.Assert;
 import org.junit.Test;
 import java.nio.file.Files;
@@ -6,19 +6,24 @@ import java.nio.file.Paths;
 public class C03_FileExist {
     @Test
     public void isExistTest(){
-        String userDIR = System.getProperty("user.dir");
-        System.out.println(userDIR);///Users/techproed/IdeaProjects/B103MavenJUnit
-        String userHOME =  System.getProperty("user.home");
-        System.out.println(userHOME);///Users/techproed
+        String userDIR = System.getProperty("user.dir"); //icinde bulunulan klasörün yolunu (Path) verir
+        System.out.println(userDIR); // /Users/macmac/IdeaProjects/com.Maven_Junit
+
+        String userHOME =  System.getProperty("user.home"); //bilgisayarimizda bulunan user klasörünü verir
+        System.out.println(userHOME);// /Users/macmac
+
         //logo.jpeg indirip MASAUSTUNE kaydedin
-//        Logo masaustunde : /Users/techproed/Desktop
+        //Logo masaustunde : /Users/macmac/Desktop
         String dosyaYolu = userHOME +"/Desktop/logo.jpeg";
-//        String dosyaYolu = userHOME +"\\Desktop\\logo.jpeg";
+
+        //String dosyaYolu = userHOME +"\\Desktop\\logo.jpeg";
         System.out.println(dosyaYolu);
-        boolean isExist = Files.exists(Paths.get(dosyaYolu));//doyda var ise tru, yok ise false
+        boolean isExist = Files.exists(Paths.get(dosyaYolu)); //dosyada var ise true, yok ise false
         Assert.assertTrue(isExist);
+
         /*
-        1. fail -> PATH YANLIS : dosya masa ustunde degil, dosya adi yanlis, yada arada onedrive gibi ex dosyalar olarak
+        fail verirse -> PATH YANLIS : dosya masa ustunde degil, dosya adi yanlis, ya da arada onedrive gibi ex dosyalar olarak
          */
+
     }
 }
--- src/test/java/homework/H03_FakerClass.java
+++ src/test/java/homework/H03_FakerClass.java
@@ -1,2 +1,50 @@
-package homework;public class H03_FakerClass {
+package homework;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class H03_FakerClass extends TestBase {
+
+    @Test
+    public void FakerClassTest() {
+
+        //1."https://facebook.com" Adresine gidin
+        driver.get("https://facebook.com");
+        Faker faker = new Faker();
+
+        //2."create new account" butonuna basin
+        driver.findElement(By.xpath("//*[text( )='Create New Account']")).click();
+
+        //3."firstName" giris kutusuna bir isim yazin
+        driver.findElement(By.xpath("//*[@name='firstname']")).click();
+        String fName = faker.name().firstName();
+        System.out.println(fName);
+
+        //4."surname" giris kutusuna bir soyisim yazin
+        driver.findElement(By.xpath("//*[@name='lastname']"));
+        String lName = faker.name().lastName();
+        System.out.println(lName);
+
+        //5. "email" giris kutusuna bir email yazin
+        driver.findElement(By.xpath("//*[@name='reg_email__']"));
+        System.out.println(faker.internet().emailAddress());
+
+        //6. "email" onay kutusuna emaili tekrar yazin
+
+
+        //7. Bir sifre girin
+        //8. Tarih icin gun secin
+        //9. Tarih icin ay secin
+        //10. Tarih icin yil secin
+        //11. Cinsiyeti secin
+        //12. Isaretlediginiz cinsiyetin secili, diger cinsiyet kutusunun secili olmadigini test edin.
+        //13. Sayfayi kapatin
+
+
+
+
+    }
+
 }
